38875d11e2328d5b6f20c0d73dd7ab04a105ef59
Patrick Wendlandt <p.wendlandt@gmx.de>
2023-11-14 16:58:13 +0100

    Add prettier and format existing files

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress'
 
 export default defineConfig({
   projectId: 'nntr7c',
@@ -10,8 +10,8 @@ export default defineConfig({
 
   component: {
     devServer: {
-      framework: "react",
-      bundler: "vite",
+      framework: 'react',
+      bundler: 'vite',
     },
   },
-});
+})
