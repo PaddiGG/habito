e6ed65befba2eba7cc27c4e766d5d98a9b35aae4
Patrick Wendlandt <p.wendlandt@gmx.de>
2024-01-22 23:19:56 +0100

    Add label to checkbox

--- src/components/Card.cy.tsx
+++ src/components/Card.cy.tsx
@@ -1,9 +1,14 @@
 import Card from './Card'
 
-it('card contains checkbox', () => {
+it('contains checkbox', () => {
   cy.mount(<Card />)
   cy.get("[data-testid^='card']")
     .find('input')
     .should('have.length', 1)
     .and('have.attr', 'type', 'checkbox')
+})
+
+it('contains checkbox', () => {
+  cy.mount(<Card />)
+  cy.get("[data-testid^='card']").find('label').should('have.length', 1)
 })
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,10 +2,13 @@ const Card = () => {
   return (
     <div
       data-testid='card'
-      className='bg-blue-900 text-white rounded-md flex px-4 py-2 space-x-4'
+      className='bg-stone-800 text-white rounded-md px-4 py-2 flex items-center space-x-4 w-full max-w-md justify-between'
     >
-      <div>Card Title</div>
-      <input type='checkbox' />
+      <label className='font-bold' htmlFor='todoCheck'>
+        Some Thing
+      </label>
+      {/* TODO: Add Card Styles  */}
+      <input type='checkbox' id='todoCheck' />
     </div>
   )
 }
